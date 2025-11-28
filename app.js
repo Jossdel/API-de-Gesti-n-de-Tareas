cd9b10d4d00af9c03ac22cedcf782e3ff6671039
Jossdel <168942707+Jossdel@users.noreply.github.com>
2025-11-28 14:34:00 -0400

    Modifying app.js

--- app.js
+++ app.js
@@ -5,30 +5,6 @@ connection();
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.get("/help", (req, res) => {
-  res.json({
-    message: "Centro de ayuda de la API",
-    routes: {
-      home: {
-        method: "GET",
-        route: "/",
-        description: "Verifica que el servidor está funcionando",
-      },
-      createUser: {
-        method: "POST",
-        route: "/createuser",
-        description: "Crear un nuevo usuario",
-      },
-      findUser: {
-        method: "GET",
-        route: "/user/:id",
-        description: "Buscar un usuario por ID",
-      },
-    },
-    author: "Josué Delgadillo",
-    version: "1.0",
-  });
-});
 
 app.use(router);
 const server = process.env.PORT || 3000;
